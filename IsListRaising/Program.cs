4d030747a53521e44d96aef110cd42b0026c6470
mitya <dsladkov@gmail.com>
2024-09-08 15:32:18 +0300

    Triangle project and new IsListRaising project were added to one sln

---
+++ IsListRaising/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+System.Console.Write("Enter numbers with white space between numbers:\t");
+var arrint = ((Console.ReadLine()?.Split(' '))?.Select(x => int.Parse(x)))?.ToArray<int>();
+
+string result = "YES";
+
+for(int i = 0; i < arrint?.Length; i++){
+  if(i + 1 < arrint.Length && arrint[i] >= arrint[i + 1])
+  {
+    result = "NO";
+    break;
+  }
+}
+System.Console.WriteLine(result);
+
+
